c1499c8ca8485dfbdf21987449c9e92013d4fb2f
Jurica Bacurin <jurica.bacurin@pascom.net>
2022-05-24 11:20:08 +0200

    CL-2812 add libvpx build on windows

---
+++ subprojects/gst-plugins-good/ext/vpx/build_libvpx_win.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+export PATH=/usr/local/bin:/mingw/bin:/bin:$PATH
+
+cd /c
+mkdir -p vpx
+cd vpx
+
+git clone --depth 1 --branch v1.11.0 https://chromium.googlesource.com/webm/libvpx
+
+rm -rf build
+mkdir build
+cd build
+
+../libvpx/configure --target=x86_64-win64-vs16 --enable-pic --as=nasm --disable-unit-tests --size-limit=16384x16384 --enable-postproc --enable-multi-res-encoding --enable-temporal-denoising --enable-vp9-temporal-denoising --enable-vp9-postproc --disable-tools --disable-examples --disable-docs
+make
+
+
+powershell.exe -Command '&{Import-Module "C:\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 62da6021 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"} dir'
+C:/BuildTools/MSBuild/Current/Bin/MSBuild.exe vpx.sln -m -t:Build -p:Configuration="Release" -p:Platform="x64"
